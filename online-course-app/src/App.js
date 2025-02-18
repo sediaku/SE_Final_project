9ac6843c9e7c25767508e74872ac4e4360515d9b
Sedinam Senaya <senayasedinam@gmail.com>
2025-02-18 20:45:51 +0000

    up

--- online-course-app/src/App.js
+++ online-course-app/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
