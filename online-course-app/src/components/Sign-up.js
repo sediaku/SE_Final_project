2f517e77ab4a420ca8bc2095ea95c8e21fc50721
Sedinam Senaya <senayasedinam@gmail.com>
2025-02-18 14:25:58 +0000

    update sign-up

--- online-course-app/src/components/Sign-up.js
+++ online-course-app/src/components/Sign-up.js
@@ -5,4 +5,40 @@ const Signup = () => {
     const[password, setPassword] = useState("");
     const[confirmPassword, setConfirmPassword] = useState("");
 
-}
+    const handleSignup = (e) => {
+        e.preventDefault();
+        console.log("Signup email:", email, "password:", password)
+    };
+
+    return(
+        <div>
+            <h2>Signup</h2>
+            <form onSubmit={handleSignup}>
+                <input
+                    type = "email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type = "password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                
+                <input
+                    type = "password"
+                    placeholder="Confirm your password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    );
+
+};
