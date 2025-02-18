c4f092fcc498866d4389e938c75ed3e1317e88aa
Sedinam Senaya <senayasedinam@gmail.com>
2025-02-18 14:06:47 +0000

    Sign-up component

---
+++ online-course-app/src/components/Sign-up.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+const Signup = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[confirmPassword, setConfirmPassword] = useState("");
+
+}
